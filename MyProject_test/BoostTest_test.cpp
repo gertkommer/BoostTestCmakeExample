1ddb52cfe234ec77e0ea8555ca0df4822ec2526d
Gert Kommer <gert.kommer@usoft.com>
2020-04-14 16:27:20 +0200

    Initial

---
+++ MyProject_test/BoostTest_test.cpp
@@ -0,0 +1,15 @@
+﻿// BoostTest_test.cpp : Defines the entry point for the application.
+//
+
+#define BOOST_TEST_MODULE MyBoostModule
+
+#include <boost/test/unit_test.hpp>
+#include "../MyProject/BoostTest.h"
+
+BOOST_AUTO_TEST_CASE(MyTestCase)
+{
+   auto integers = { 1, 2, 3, 4, 5 };
+   auto result = sum_integers(integers);
+   BOOST_REQUIRE(result == 15);
+}
+
